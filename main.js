ec0b24700f7727f57e22266e704b6ce497b1c566
qqmeloon <m.pastoral19@gmail.com>
2023-12-23 23:19:07 +0800

    update

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
-
 const enableScroll = () => {
-  document.body.classList.remove("disable-scrolling")
-}
+  document.body.classList.remove("disable-scrolling");
+};
 
-setTimeout(enableScroll, 7500)
+setTimeout(enableScroll, 7500);
 
 const moveIns = document.querySelectorAll(".fade-in");
 
@@ -30,3 +28,47 @@ appearOptions);
 moveIns.forEach((moveIn) => {
   appearOnScroll.observe(moveIn);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Update styles and required status based on the initial checked state
+  updateStyles();
+  updateRequiredStatus();
+
+  // Add event listener for changes in the checked state
+  document
+    .querySelectorAll('.button-div input[type="radio"]')
+    .forEach(function (radio) {
+      radio.addEventListener("change", function () {
+        updateStyles();
+        updateRequiredStatus();
+        updateInvalidFeedbackVisibility();
+      });
+    });
+});
+
+function updateStyles() {
+  document
+    .querySelectorAll('.button-div input[type="radio"]')
+    .forEach(function (radio) {
+      const label = document.querySelector('label[for="' + radio.id + '"]');
+      label.classList.toggle("checked", radio.checked);
+    });
+}
+
+function updateRequiredStatus() {
+  const rejectRadio = document.getElementById("flexRadioDefault1");
+  const textarea = document.getElementById("floatingTextarea");
+  textarea.required = rejectRadio.checked;
+}
+
+function updateInvalidFeedbackVisibility() {
+  const rejectRadio1 = document.getElementById("flexRadioDefault1");
+  const rejectRadio3 = document.getElementById("flexRadioDefault3");
+  const invalidFeedback = document.querySelector(".invalid-feedback");
+
+  if (rejectRadio1.checked || rejectRadio3.checked) {
+    invalidFeedback.style.display = "block";
+  } else {
+    invalidFeedback.style.display = "none";
+  }
+}
